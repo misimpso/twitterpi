eb7ef4f7d2f6fa8f43ff411647aa14fcab6cbb73
Michael Simpson <mikeysimpson4@gmail.com>
2021-10-17 23:55:29 +0000

    Add main entry point for twitterp program

--- src/twitterpi/main.py
+++ src/twitterpi/main.py
@@ -1,4 +1,3 @@
-import aiohttp
 import asyncio
 import toml
 
@@ -12,6 +11,7 @@ class TwitterBot:
     def load_accounts(self, creds_path: Path) -> list[Account]:
         """ TODO: docstring
         """
+
         creds_dict = self.__read_creds_file(creds_path)
         accounts = self.__initialize_accounts(creds_dict)
         return accounts
@@ -19,25 +19,22 @@ class TwitterBot:
     def run(self, accounts: list[Account]):
         """ TODO: docstring
         """
+
         loop = asyncio.get_event_loop()
         loop.run_until_complete(asyncio.gather(*[account.start() for account in accounts]))
 
-    # async def main(self, accounts: list[Account]):
-    #     """ TODO: docstring
-    #     """
-    #     for account in accounts:
-    #         await account.start()
-
-        
     def __read_creds_file(self, creds_path: Path) -> dict[str, str]:
         """ TODO: docstring
         """
+
         print(f"Loading account credentials ... [Path: {creds_path}]")
         if not creds_path.is_file():
             raise FileNotFoundError(f"Cannot load account credentials. [{creds_path}]")
+
         account_creds = {}
         with creds_path.open("r") as f:
             account_creds = toml.load(f)
+
         print("Account credentials loaded!")
         return account_creds
     
@@ -59,7 +56,11 @@ class TwitterBot:
         return accounts
 
 
-if __name__ == "__main__":
+def main():
     bot = TwitterBot()
     accounts = bot.load_accounts(CREDS_PATH)
     bot.run(accounts)
+
+
+if __name__ == "__main__":
+    main()
