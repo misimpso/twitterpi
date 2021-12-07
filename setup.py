79fb384d64fcdc9fb902f9691acccb8968f5b985
Michael Simpson <mikeysimpson4@gmail.com>
2021-12-07 07:03:43 +0000

    Add docstring to read_file method in setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,17 @@ from setuptools import setup
 REQUIREMENTS_FILE = Path(__file__).parent / "requirements.txt"
 REQUIREMENTS_DEV_FILE = Path(__file__).parent / "requirements-dev.txt"
 
+
 def read_file(filepath: Path) -> list[str]:
+    """ Read given `filepath` and return contents as list of strings.
+
+    Args:
+        filepath (obj: Path): Path to file.
+    
+    Returns:
+        list[str]: File contents as list of strings.
+    """
+
     contents = []
     with filepath.open("r") as f:
         contents = f.readlines()
@@ -13,6 +23,8 @@ def read_file(filepath: Path) -> list[str]:
 
 
 setup(
-    extras_requires=read_file(REQUIREMENTS_DEV_FILE),
     install_requires=read_file(REQUIREMENTS_FILE),
+    extras_require={
+        "test": read_file(REQUIREMENTS_DEV_FILE),
+    },
 )
