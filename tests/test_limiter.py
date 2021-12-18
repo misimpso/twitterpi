d587b2297337d84058758d9aa0cf5b1e4956ae8c
Michael Simpson <mikeysimpson4@gmail.com>
2021-12-18 07:31:21 +0000

    Swap datetime usage for perfcounter in Limiter test

--- tests/test_limiter.py
+++ tests/test_limiter.py
@@ -2,7 +2,7 @@ import asyncio
 import unittest
 
 from contextlib import closing
-from datetime import datetime
+from time import perf_counter
 from twitterpi.limiter import Limiter
 from unittest.mock import Mock, patch
 
@@ -21,13 +21,13 @@ class LimiterTests(unittest.TestCase):
 
         mock_logger = Mock()
         test_limiter = Limiter(requests_per_day=requests_per_day)
-        call_times: datetime = []
+        call_times: float = []
 
         async def add_current_time(self):
             """ Test method to get the time between calls
             """
 
-            call_times.append(datetime.now())
+            call_times.append(perf_counter())
 
         mock_func = test_limiter.acquire(add_current_time)
 
@@ -42,9 +42,9 @@ class LimiterTests(unittest.TestCase):
         # Make sure the sum of the time spent "sleeping" and the elapsed time between calls, is more
         # than `seconds_per_request`.
         for i, call in enumerate(mock_sleep.call_args_list):
-            sleep_time = call[0][0]
-            elapsed_call_time = (call_times[i + 1] - call_times[i]).total_seconds()
-            self.assertTrue((sleep_time + elapsed_call_time) >= seconds_per_request)
+            sleep_time: float = call[0][0]
+            elapsed_call_time: float = (call_times[i + 1] - call_times[i])
+            self.assertGreaterEqual((sleep_time + elapsed_call_time), seconds_per_request)
 
 
 if __name__ == "__main__":
